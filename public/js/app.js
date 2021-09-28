ac16b04480fe5d1c97bc5c3b53c147405d5d7771
Clarice Deluchi <cdeluchi@gmail.com>
2021-09-28 11:29:22 +0200

    part 2/1

--- public/js/app.js
+++ public/js/app.js
@@ -5,21 +5,39 @@ Vue.createApp({
         return {
             name: "Image Board",
             images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null,
         };
     },
     mounted() {
         console.log("MOUNTED");
-        fetch("/images")
+        fetch("/images") //make http request
             .then((response) => response.json())
             .then(({ rows }) => {
-                console.log("images", rows);
+                // console.log("images", rows);
                 this.images = rows;
-            });
+            })
+            .catch(console.log);
     },
     methods: {
-        setName(images) {
-            console.log(images);
-            this.name = images;
+        clickHandler() {
+            const fd = new FormData();
+            fd.append("title", this.title);
+            fd.append("description", this.description);
+            fd.append("username", this.username);
+            fd.append("file", this.file);
+            fetch("/upload", {
+                method: "POST",
+                body: fd,
+            })
+                .then((response) => response.json())
+                .then((result) => console.log(result))
+                .catch((err) => console.log(err));
+        },
+        fileSelectHandler(e) {
+            this.file = e.target.files[0];
         },
     },
 }).mount("#main");
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const { uploader } = require("./upload");
 
 app.use(express.static("./public"));
-
 app.use(express.json());
 
-app.get("/images", (req, res) => {
+app.post("/upload", uploader.single("file"), (req, res) => {
     console.log("req.body", req.body);
+    console.log("req.file", req.file);
+    if (req.file) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
+
+app.get("/images", (req, res) => {
+    // console.log("req.body", req.body);
     db.getImage().then(({ rows }) => {
-        console.log("rows", rows);
+        // console.log("rows", rows);
         return res.json({ rows });
     });
 });
